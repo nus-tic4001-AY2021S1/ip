4655c6b961211c0d8e1e04f67feaf7335081f582
Lai Ping <chilaiping@icloud.com>
2020-08-15 13:35:33 +0800

    Level 0 - Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        GreetNote("welcome");
+        GreetNote("bye");
+    }
+
+    private static void GreetNote(String hi_bye) {
+
+        String welcome = "____________________________________________________________\n"
+                        + "Hello! I'm Duke\n"
+                        + "What can I do for you?\n"
+                        + "____________________________________________________________\n";
+
+        String farewell = "Bye. Hope to see you again soon!\n"
+                        + "____________________________________________________________";
+
+        if(hi_bye.equals("welcome")) {
+            System.out.println(welcome);
+        } else {
+            System.out.println(farewell);
+        }
     }
 }
