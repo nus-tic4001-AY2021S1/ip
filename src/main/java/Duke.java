181bb4548204241005093990d71beea537b9b77d
linqing42 <linqing42@hotmail.com>
2020-08-18 23:28:21 +0800

    Level 0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printWelcome();
+        showExit();
+    }
+    public static void printWelcome() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+    }
+    public static void showExit() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
