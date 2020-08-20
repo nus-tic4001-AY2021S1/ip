ac1c162d2974e622d545e4b9e0584308b196d29c
linqing42 <linqing42@hotmail.com>
2020-08-20 22:27:54 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
@@ -8,12 +9,25 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         printWelcome();
+        //Declare the object and initialize with
+        //predefined standard input object
+        Scanner in = new Scanner(System.in);
+
+        //String input
+        String input = in. nextLine();
+
+        while(!input.equals("bye")){
+            System.out.println("     " + input);
+            input =in.nextLine();
+        }
+
         showExit();
     }
     public static void printWelcome() {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
     }
     public static void showExit() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("   Bye. Hope to see you again soon!");
     }
+
 }
