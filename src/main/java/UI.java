35d92ca36e84bc46ec46e6b4141f16e93d1d82d6
Impala36 <zhenquan38@gmail.com>
2020-08-20 22:40:16 +0800

    Level 0. Greet

---
+++ src/main/java/UI.java
@@ -0,0 +1,15 @@
+public class UI {
+
+    public static void greet() {
+        String greeting = "____________________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n";
+        System.out.println(greeting);
+    }
+    public static void farewell() {
+        String farewell = "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(farewell);
+    }
+}
