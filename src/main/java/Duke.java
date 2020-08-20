ac359c3f3b5c927fdd73856ae6bc294de460f2b9
Lin Binhui <>
2020-08-21 00:03:17 +0800

    level-2.5 add mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,9 +10,23 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        // symbol
+        int tick1 = 10003;
+        int cross1 = 9587;
+        String tick = Character.toString((char)tick1);
+        String cross = Character.toString((char)cross1);
 
-       // creating a array list to store user input cap to 100
+        System.out.println(tick);
+        System.out.println(cross);
+
+
+
+
+
+        // creating array
         ArrayList commandName = new ArrayList(100);
+        ArrayList check = new ArrayList(100);
+
 
 
         // Welcome msg
@@ -23,25 +37,46 @@ public class Duke {
 
         while (myObj.hasNext()) {
 
+           // System.out.println("Added task press 1");
+           // System.out.println("Show current task press 2");
+
+
+
+
             String obj1 = myObj.nextLine();
 
             if(obj1.equalsIgnoreCase("bye")){
                 break;
             }
             else if(obj1.equalsIgnoreCase("list")){
+                System.out.println("Here are the tasks in your list:");
                 int number = 1;
                 for( int i = 0; i < commandName.size(); i++){
-                    System.out.println(number + "."+" "+commandName.get(i));
+                    System.out.println(number +"." +"["+check.get(i)+"]"+" "+commandName.get(i));
                     number++;
                 }
             }
+            else if(obj1.equalsIgnoreCase("done 2")){
+
+                //remove all characters in the string
+                String clean = obj1.replaceAll("\\D+","");
+                int i = Integer.parseInt(clean);
+                //System.out.println(i);
+                check.set(i-1,tick);
+
+
+
+            }
             else{
+
+                //job = "[" + cross + "]" + obj1;
                 System.out.println("added: " + obj1);
                 commandName.add(obj1);
+                check.add(cross);
             }
         }
 
-        
+
         //Good Bye msg
         System.out.println("Bye. Hope to see you again soon!");
 
