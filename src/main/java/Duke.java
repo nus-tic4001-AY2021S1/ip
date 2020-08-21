876784f69e2f66c93a76092c93bf5f115c81b82d
Timmy <walkawayly@gmail.com>
2020-08-21 20:14:32 +0800

    Level 1 Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 public class Duke {
+
+    public static List<TaskDTO> taskList = new ArrayList<>();
+    public static int taskId = 0;
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +14,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+
+
+
     }
+
+
 }
