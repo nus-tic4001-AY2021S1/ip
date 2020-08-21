cf0d5a86c629e4b70f935c5d22ecb44500906bb4
Adi <e0261621@u.nus.edu>
2020-08-21 20:09:43 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -31,21 +31,42 @@ public class Duke {
 
         while (!(line.contentEquals("bye") || line.contentEquals("Bye"))) {
 
-            if ((line.equals("list") || line.equals("List")) ) {
-                if(!tasks.isEmpty()){
+            if ((line.equals("list") || line.equals("List"))) {
+                if (!tasks.isEmpty()) {
                     int counter = 1;
                     for (Task t : tasks) {
 
-                        System.out.println(counter + "." + t.getDescription());
+                        System.out.print(counter + ".");
+                        if (t.isDone()) {
+                            System.out.print("[\u2713]");
+                        } else {
+                            System.out.print("[\u2717]");
+                        }
+                        System.out.print(" " + t.getDescription() + "\n");
                         counter++;
                     }
-                }else{
+                } else {
                     System.out.println("The List is empty, please add some Tasks/Todos");
                 }
+            } else if ((line.contains("done") || line.contains("Done"))) {
+
+                String taskNum[] = line.split(" ");
+                tasks.get(Integer.parseInt(taskNum[1]) - 1).setDone(true);
+
+                System.out.println(hLine);
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.print("[\u2713]");
+                System.out.print(" " + tasks.get(Integer.parseInt(taskNum[1]) - 1).getDescription()+"\n");
+
+                System.out.println(hLine);
+
             } else {
                 if (!line.isBlank()) {
+
                     System.out.println(hLine + "\n" + "Added: " + line + "\n" + hLine);
                     tasks.add(new Task(line));
+
                 }
             }
             line = in.nextLine();
