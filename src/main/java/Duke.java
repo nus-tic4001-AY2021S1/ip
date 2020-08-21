76fcb2e909580be6bac10326f06ac656fa7eb975
e0260222 <e0260222@u.nus.edu>
2020-08-21 12:49:34 +0800

    Duke class: add greeting messages
    There is no message to greet the users.
    
    Greeting messages let the users know Duke has started.
    
    Let's add hello and goodbye messages.
    
    This version fulfils Level-0 requirements.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
 public class Duke {
+    public static final String LINE = "____________________________________________________________";
     public static void main(String[] args) {
+        printWelcome();
+        printGoodbye();
+    }
+
+    public static void printWelcome() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println(LINE);
+        System.out.println(logo);
+        System.out.println("Duke: Hello! I'm Duke, your personal chatbot.");
+        System.out.println("Duke: What can I do for you?");
+        System.out.println(LINE);
+    }
+
+    public static void printGoodbye() {
+        System.out.println("Duke: Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }
