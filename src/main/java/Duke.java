0a9b12b62c4374eded8a8b96e3cc3505f0c28a89
ZhengShijie <ZhengShijie@users.noreply.github.com>
2020-08-21 19:34:36 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -13,6 +15,20 @@ public class Duke {
         String bye = " Bye. Hope to see you again soon!";
         System.out.println("________________________________________________________");
         System.out.println(hello);
+        System.out.println("________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            String input = scanner.next();
+            if(input.equalsIgnoreCase("bye")){
+                break;
+            }else{
+                System.out.println(input);
+            }
+            System.out.println("________________________________________________________");
+        }
+
         System.out.println("________________________________________________________");
         System.out.println(bye);
         System.out.println("________________________________________________________");
