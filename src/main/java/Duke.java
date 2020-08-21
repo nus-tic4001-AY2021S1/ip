9f4c91ceb8a69a797476ec527fd5009d1af8c283
Chenduo1412 <e0260233@u.nus.edu>
2020-08-21 20:44:13 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Greet greeting = new Greet();
+        greeting.greetInfo();
     }
 }
---
+++ src/main/java/Greet.java
@@ -0,0 +1,7 @@
+public class Greet {
+    public void greetInfo() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("----------------\n"+"Bye. Hope to see you again soon!");
+    }
+}
