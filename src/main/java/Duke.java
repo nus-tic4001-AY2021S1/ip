e165765ba109eddbea3473735bdd398f95abeaf5
ZhengShijie <ZhengShijie@users.noreply.github.com>
2020-08-22 10:32:07 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,6 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-
         String hello = " Hello! I'm Duke\n" +
                 " What can I do for you?";
 
