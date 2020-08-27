88eef02b052f11d15bf7ec6ef0d2acf452b169f4
Lai Ping <>
2020-08-28 00:09:14 +0800

    A-CodingStandard: Follow the Coding Standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,25 +2,23 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Global {
-    static String patternLine = "____________________________________________________________";
+    static String PATTERNLINE = "____________________________________________________________";
 }
 
 enum Action {
-    DONE,
-    LIST,
-    ADD,
-    ADD_TODO,
-    ADD_DEADLINES,
-    ADD_EVENTS,
-    INVALID,
+    Done,
+    List,
+    AddTodo,
+    AddDeadlines,
+    AddEvents,
+    Invalid,
 }
 
 public class Duke {
 
     public static void main(String[] args) {
 
-        ArrayList<Task> task = new ArrayList<>();
-        //Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -29,38 +27,32 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        GreetNote("welcome");
+        greetNote("welcome");
 
         Scanner in = new Scanner(System.in);
-        //String input= in.nextLine();
 
-        //while (!(input.toLowerCase()).equals("bye")) {
         while(in.hasNextLine()){
             String input = in.nextLine();
+
             if(!(input.toLowerCase()).equals("bye")) {
-                ProcessInput(input, task);
+                processInput(input, tasks);
             } else {
-                GreetNote("bye");
+                greetNote("bye");
                 return;
             }
-
-            //in = new Scanner(System.in);
-           //input = in.nextLine();
         }
-
-
     }
 
-    private static void GreetNote(String hi_bye) {
+    private static void greetNote(String hi_bye) {
 
-        String welcome = Global.patternLine
+        String welcome = Global.PATTERNLINE
                         + "\n     Hello! I'm Duke\n"
                         + "     What can I do for you?\n"
-                        + Global.patternLine + "\n";
+                        + Global.PATTERNLINE + "\n";
 
-        String farewell = Global.patternLine
+        String farewell = Global.PATTERNLINE
                         + "\n   Bye. Hope to see you again soon!\n"
-                        + Global.patternLine;
+                        + Global.PATTERNLINE;
 
         if(hi_bye.equals("welcome")) {
             System.out.println(welcome);
@@ -68,96 +60,100 @@ public class Duke {
             System.out.println(farewell);
         }
     }
-    private static void ProcessInput(String input, ArrayList<Task> task) {
+    private static void processInput(String input, ArrayList<Task> tasks) {
 
         String command = input.toLowerCase();
         Action action;
-        int count = task.size();
-        int index;
+        int count = tasks.size();
 
         if(command.startsWith("list")) {
-            action = Action.LIST;
+            action = Action.List;
         } else if (command.length()> 4 && command.substring(0, 4).equals("done")) {
-            action = Action.DONE;
+            action = Action.Done;
         } else if (command.length()> 5 && command.substring(0, 4).equals("todo")) {
-            action = Action.ADD_TODO;
+            action = Action.AddTodo;
         } else if (command.length()> 9 && command.substring(0, 8).equals("deadline")) {
-            action = Action.ADD_DEADLINES;
+            action = Action.AddDeadlines;
         }else if (command.length()> 6 && command.substring(0, 5).equals("event")) {
-            action = Action.ADD_EVENTS;
+            action = Action.AddEvents;
         }else {
-            action = Action.INVALID;
+            action = Action.Invalid;
         }
 
         switch(action) {
+        case List :
+            printList(tasks);
+            break;
 
-            case LIST :
-                PrintList(task);
-                break;
+        case Done :
+            markDone(command,tasks);
+            break;
 
-            case DONE :
-                //If user input done1 instead of done 1
-                if(command.contains(" ")) {
-                    index = Integer.parseInt(command.substring(5));
-                } else {
-                    index = Integer.parseInt(command.substring(4));
-                }
+        case AddTodo:
+            addTodo(tasks, input, count);
+            break;
 
-                //If user input Done item out of bound
-                if (index > task.size() || index == 0) {
-                    System.out.println(Global.patternLine + "\nFriend, You do not have so much task in the list\n" + Global.patternLine);
-                    return;
-                }
+        case AddDeadlines:
+            addDeadlines(tasks, input, count);
+            break;
 
-                task.get(index-1).markAsDone();
-                System.out.println(Global.patternLine + "\nNice! I've marked this task as done:");
-                task.get(index-1).printTask();
-                System.out.println(Global.patternLine);
-                break;
+        case AddEvents:
+            addEvents(tasks, input, count);
+            break;
 
-            case ADD_TODO:
-                AddTodo(task, input, count);
-                break;
-
-            case ADD_DEADLINES:
-                AddDeadlines(task, input, count);
-                break;
-
-            case ADD_EVENTS:
-                AddEvents(task, input, count);
-                break;
-
-            default:
-                System.out.println(Global.patternLine + "\nYou have entered invalid input. Please re-input or \n" +
-                        "Enter bye to terminate the program.\n" + Global.patternLine);
+        default:
+            System.out.println(Global.PATTERNLINE + "\nYou have entered invalid input. Please re-input or \n" +
+                    "Enter bye to terminate the program.\n" + Global.PATTERNLINE);
 
         }
     }
-    private static void ReplyLine(ArrayList<Task> task, String input, int count) {
-        System.out.print(Global.patternLine + "\nGot it. I've added this task: \n\t" );
-        task.get(count).printTask();
+    private static void replyLine(ArrayList<Task> tasks, String input, int count) {
+        System.out.print(Global.PATTERNLINE + "\nGot it. I've added this task: \n\t" );
+        tasks.get(count).printTask();
         count += 1;
-        System.out.println("Now you have " + count + " tasks in the list.\n" + Global.patternLine );
+        System.out.println("Now you have " + count + " tasks in the list.\n" + Global.PATTERNLINE );
     }
-    private static void PrintList(ArrayList<Task> task) {
-        if(task.size() == 0) {
-            String noItem = Global.patternLine + "\nYour List is Empty\n" + Global.patternLine;
+    private static void printList(ArrayList<Task> tasks) {
+        if(tasks.size() == 0) {
+            String noItem = Global.PATTERNLINE + "\nYour List is Empty\n" + Global.PATTERNLINE;
             System.out.println(noItem);
             return;
         }
-        System.out.println(Global.patternLine);
-        for(int i=1; i<=task.size(); i++) {
+        System.out.println(Global.PATTERNLINE);
+        for(int i = 1; i <= tasks.size(); i++) {
             System.out.print(i + ". ");
-            task.get(i-1).printTask();
+            tasks.get(i-1).printTask();
         }
-        System.out.println(Global.patternLine);
+        System.out.println(Global.PATTERNLINE);
+    }
+    private static void markDone(String command, ArrayList<Task> tasks) {
+        int index = 0;
+
+        // If user input done1 instead of done 1
+        if(command.contains(" ")) {
+            index = Integer.parseInt(command.substring(5));
+        } else {
+            index = Integer.parseInt(command.substring(4));
+        }
+
+        // If user input Done item out of bound
+        if (index > tasks.size() || index == 0) {
+            System.out.println(Global.PATTERNLINE + "\nFriend, You do not have so much task in the list\n"
+                    + Global.PATTERNLINE);
+            return;
+        }
+
+        tasks.get(index-1).markAsDone();
+        System.out.println(Global.PATTERNLINE + "\nNice! I've marked this task as done:");
+        tasks.get(index-1).printTask();
+        System.out.println(Global.PATTERNLINE);
     }
 
-    private static void AddTodo(ArrayList<Task> task, String input, int count) {
-        task.add(new ToDos(input.substring(5)));
-        ReplyLine(task, input, count);
+    private static void addTodo(ArrayList<Task> tasks, String input, int count) {
+        tasks.add(new ToDos(input.substring(5)));
+        replyLine(tasks, input, count);
     }
-    private static void AddDeadlines(ArrayList<Task> task, String input, int count) {
+    private static void addDeadlines(ArrayList<Task> tasks, String input, int count) {
         int index = input.indexOf("/by");
         if(index == -1) {
             System.out.println("You have not input your Schedule or Wrong format. \nPlease include -> /by when");
@@ -165,19 +161,21 @@ public class Duke {
         }
         String schedule = input.substring(index + 4);
         String taskName = input.substring(input.indexOf(" ")+1, index);
-        task.add(new Deadlines(taskName, schedule));
-        ReplyLine(task, input, count);
+        tasks.add(new Deadlines(taskName, schedule));
+        replyLine(tasks, input, count);
     }
-    private static void AddEvents(ArrayList<Task> task, String input, int count){
+    private static void addEvents(ArrayList<Task> tasks, String input, int count){
         int index = input.indexOf("/at");
         String schedule, taskName;
+
         if(index == -1) {
             System.out.println("You have not input your Schedule or Wrong format. \nPlease include -> /at when");
             return;
         }
         schedule = input.substring(index + 4);
         taskName = input.substring(input.indexOf(" ")+1, index);
-        task.add(new Events(taskName, schedule));
-        ReplyLine(task, input, count);
+        tasks.add(new Events(taskName, schedule));
+        replyLine(tasks, input, count);
     }
+
 }
