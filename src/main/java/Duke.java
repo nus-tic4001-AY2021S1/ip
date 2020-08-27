8df50f10180a187de2744db4d235f117444b08e2
Januarius Jang <>
2020-08-28 00:27:52 +0800

    added Task class. Add mark as done function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) throws IOException {
 
-        ArrayList<String> taskList = new ArrayList<String>();
+        ArrayList<Task> taskList = new ArrayList<Task>();
 
         System.out.println("Hello! I'm Duke\n" );
         System.out.println("What can I do for you?\n" );
@@ -16,16 +16,27 @@ public class Duke {
 
         while(!textInput.equals("bye")) {
 
+            String[]arrayOfStr=textInput.split(" ",2);
+
             if (textInput.equals("list")) {
                 //System.out.println(taskList);
                 for (int i = 0; i < taskList.size();i++)
                 {
-                    System.out.println(i+1 + ". " +taskList.get(i));
+                    System.out.println(i+1 + ". "+ "[" +taskList.get(i).getStatusIcon() +"] "  +taskList.get(i).getDescription());
                 }
             }
+            else if(arrayOfStr[0].equals("done")){
+                //System.out.println("Done detected");
+                int index=Integer.parseInt(arrayOfStr[1])-1;
+                taskList.get(index).markAsDone();
+                System.out.println("Nice! I've marked this task as done: "+ "[" +taskList.get(index).getStatusIcon() +"] "  +taskList.get(index).getDescription());
+
+
+            }
             else {
-                taskList.add((textInput));
-                System.out.println("added: " + textInput);
+                Task t=new Task(textInput);
+                taskList.add((t));
+                System.out.println("added: " + t.getDescription());
             }
             textInput = reader.readLine();
         }
