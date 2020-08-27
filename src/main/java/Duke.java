575ae4884dfd9bb096e7b16f53d78c9403342c40
Januarius Jang <>
2020-08-27 22:36:37 +0800

    program exit using Bye keyword

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("Hello! I'm Duke\n" );
         System.out.println("What can I do for you?\n" );
-        System.out.println("Bye. Hope to see you again soon!\n" );
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String textInput = reader.readLine();
+
+        while(!textInput.equals("bye")){
+            System.out.println(textInput);
+            textInput = reader.readLine();
+        }
+        System.out.println(" Bye.Hope to see you again soon!\n" );
+
+
+
+
+
 
     }
 }
