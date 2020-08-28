96eeb6034ed2341936365fb2101a113ba4a9b6be
Dai <6608118@gamil.com>
2020-08-28 23:36:19 +0800

    add: Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "  __          __ \n"
+                + "  | | ____    | | ____\n"
+                + "  | |/ __ \\   | |/ __ \\\n"
+                + "__/ | |__| |__/ | |__| |\n"
+                + "\\__/ \\____/ \\__/ \\____/\n";
         System.out.println("Hello from\n" + logo);
+        Ui ui = new Ui();
+        ui.greetUser();
+        ui.farewellUser();
     }
 }
