33164cf4deeed344ff058d59410f38e42d683ad9
Dai <6608118@gamil.com>
2020-08-28 23:39:02 +0800

    add: Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "  __          __ \n"
@@ -6,8 +8,23 @@ public class Duke {
                 + "__/ | |__| |__/ | |__| |\n"
                 + "\\__/ \\____/ \\__/ \\____/\n";
         System.out.println("Hello from\n" + logo);
-        Ui ui = new Ui();
-        ui.greetUser();
-        ui.farewellUser();
+        System.out.println("____________________________________________________________\n"
+                + "Hi! I'm Jojo\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String input = scanner.next();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        System.out.println("Bye, have a nice day!\n"
+                + "____________________________________________________________");
     }
 }
