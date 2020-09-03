6045c175ea824f94da2730a58507687097f0d6da
linqing42 <linqing42@hotmail.com>
2020-09-04 02:22:22 +0800

    add empty list and done for handle error

--- src/main/java/Dukes/Task.java
+++ src/main/java/Dukes/Task.java
@@ -23,31 +23,7 @@ public class Task {
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
     }
-    public static void markDone(int arrayIndex) {
-        ArrayList<Task> tasks = new ArrayList<>();
-        Task task = tasks.get(arrayIndex);
-        task.setDone();
-        System.out.println("     Nice! I've marked this task as done:\n"
-                + task
-                + "\n"
-                + "___________________________________________________________________\n");
-    }
 
-    public static void markAsDone(String input) {
-        try {
-            if (input.length() < 6) {
-                throw new DukeException("please key in correct format.");
-            } else {
-                String listActionIndex = (input.split(" "))[1];
-                int arrayIndex = Integer.parseInt(listActionIndex) - 1;
-                markDone(arrayIndex);
-            }
-        }catch (DukeException e) {
-            System.out.println(e.getMessage());
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Please key in a number from the list.");
-        }
-    }
 }
 
 
