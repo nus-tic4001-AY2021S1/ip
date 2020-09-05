87c5b48416e51ca4fd7d4cdeb9dfda0ba2a10a27
Januarius Jang <>
2020-09-06 00:01:55 +0800

    committing text UI files and some initial error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,6 +7,7 @@ public class Duke {
     public static void main(String[] args) throws IOException {
 
         ArrayList<Task> taskList = new ArrayList<Task>();
+        String commandKeyword;
 
         System.out.println("Hello! I'm Duke\n" );
         System.out.println("What can I do for you?\n" );
@@ -16,7 +17,8 @@ public class Duke {
 
         while(!textInput.equals("bye")) {
 
-            String[]arrayOfStr=textInput.split(" ",2);
+                String[]arrayOfStr=textInput.split(" ",2);
+
 
             switch (arrayOfStr[0]) {
                 case "list":
@@ -30,37 +32,50 @@ public class Duke {
                     int index=Integer.parseInt(arrayOfStr[1])-1;
                     taskList.get(index).markAsDone();
                     System.out.println("Nice! I've marked this task as done: "+ "[" +taskList.get(index).getStatusIcon() +"] "  +taskList.get(index).toString());
+
                     break;
 
                 case "deadline":
-                    String[]arrayOfDoneDetails=arrayOfStr[1].split("by ",2);
-                    Task d=new Deadline(arrayOfDoneDetails[0],arrayOfDoneDetails[1] );
-                    taskList.add((d));
-                    System.out.println("Got it. I've added this task: \n" + d.toString());
-                    System.out.println("Now you have " + taskList.size()+ " tasks in the list");
+                    try{
+                        String[]arrayOfDoneDetails=arrayOfStr[1].split("by ",2);
+                        Task d=new Deadline(arrayOfDoneDetails[0],arrayOfDoneDetails[1] );
+                        taskList.add((d));
+                        System.out.println("Got it. I've added this task: \n" + d.toString());
+                        System.out.println("Now you have " + taskList.size()+ " tasks in the list");
+                    }
+                    catch (ArrayIndexOutOfBoundsException exception) {
+                        System.out.println("Sorry, you will need to enter a description of the Todo task");
+                    }
                     break;
 
                 case "event":
-                    String[]arrayOfEventDetails=arrayOfStr[1].split("at ",2);
-                    //arrayOfDoneDetails[0] contains description, arrayOfDoneDetails[1] contains the date.
-                    //System.out.println("by: " + arrayOfDoneStr[1]);
-                    Task e=new Event(arrayOfEventDetails[0],arrayOfEventDetails[1] );
-                    taskList.add((e));
-                    System.out.println("Got it. I've added this task: \n" + e.toString());
-                    System.out.println("Now you have " + taskList.size()+ " tasks in the list");
+                    try{
+                        String[]arrayOfEventDetails=arrayOfStr[1].split("at ",2);
+                        Task e=new Event(arrayOfEventDetails[0],arrayOfEventDetails[1] );
+                        taskList.add((e));
+                        System.out.println("Got it. I've added this task: \n" + e.toString());
+                        System.out.println("Now you have " + taskList.size()+ " tasks in the list");
+                    }
+                    catch (ArrayIndexOutOfBoundsException exception) {
+                        System.out.println("Sorry, you will need to enter a description or details for the Event");
+                    }
                     break;
-
                 case "todo":
-                    Task t=new Todo(arrayOfStr[1]);
-                    taskList.add((t));
-                    System.out.println("Got it. I've added this task: \n" + t.toString());
-                    System.out.println("Now you have " + taskList.size()+ " tasks in the list");
+                    try{
+                        Task t=new Todo(arrayOfStr[1]);
+                        taskList.add((t));
+                        System.out.println("Got it. I've added this task: \n" + t.toString());
+                        System.out.println("Now you have " + taskList.size()+ " tasks in the list");
+                    }
+                    catch (ArrayIndexOutOfBoundsException exception) {
+                        System.out.println("Sorry, you will need to enter a description of the Todo task");
+                    }
                     break;
 
                 default:
-                    System.out.println(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
-            
+
             textInput = reader.readLine();
         }
 
