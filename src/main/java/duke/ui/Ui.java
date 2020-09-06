6f18ff4167a4cfde4ebf08eecb2be93541f39ca0
Impala36 <zhenquan38@gmail.com>
2020-09-06 18:12:34 +0800

    Level 6. Delete

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -12,12 +12,13 @@ public class Ui {
         String greeting = "Hello! I'm Duke.\n"
                 + "I can help you manage a list of tasks!\n"
                 + "What you can tell me to do is listed below:\n"
-                + "  ⬡  Create a Todo task       | " + colorGreen("todo <your task>\n")
-                + "  ⬢  Create a Deadline task   | " + colorGreen("deadline <your task> /by <when>\n")
-                + "  ⬡  Create an Event task     | " + colorGreen("event <your task> /at <when to when>\n")
-                + "  ⬢  Mark a task as completed | " + colorGreen("done <task index number>\n")
-                + "  ⬡  List down all tasks      | " + colorGreen("list\n")
-                + "  ⬢  Exit my program          | " + colorGreen("bye or hit Enter\n")
+                + "  ⬡  Create a Todo task     | " + colorGreen("todo <your task>\n")
+                + "  ⬢  Create a Deadline task | " + colorGreen("deadline <your task> /by <when>\n")
+                + "  ⬡  Create an Event task   | " + colorGreen("event <your task> /at <when to when>\n")
+                + "  ⬢  Complete a task        | " + colorGreen("done <task index number>\n")
+                + "  ⬡  Delete a task          | " + colorGreen("delete <task index number>\n")
+                + "  ⬡  List down all tasks    | " + colorGreen("list\n")
+                + "  ⬢  Exit my program        | " + colorGreen("bye or hit Enter\n")
                 + "So, what would you like to start with?";
         printBorderlines(greeting);
     }
@@ -27,14 +28,21 @@ public class Ui {
         printBorderlines(farewell);
     }
 
-    public void printTaskAdded(String line, TaskList tasks) {
+    public void printTaskAdded(TaskList tasks) {
         String added = "Great! You have entered a proper task as shown below:\n"
-                + colorGreen(line)
-                + "\nWe now have " + tasks.size() + " task(s) in your list!";
+                + colorGreen(tasks.get(tasks.size() - 1).getDescription()
+                + "\nWe now have " + tasks.size() + " task(s) in your list!");
         printBorderlines(added);
     }
 
-    public void printTaskDone(int index, TaskList tasks) {
+    public void printTaskRemoved(TaskList tasks) {
+        String added = "Okay! We have removed the task as shown below:\n"
+                + colorRed(tasks.get(tasks.size() - 1).getDescription()
+                + colorGreen("\nWe now have " + (tasks.size() - 1) + " task(s) in your list!"));
+        printBorderlines(added);
+    }
+
+    public void printTaskCompleted(int index, TaskList tasks) {
         String done = "Good work there! Now we have:\n"
                 + "\033[33mTask [" + index + "] "
                 + tasks.get(index - 1).getDescription() + " set as completed!\033[0m";
@@ -60,6 +68,7 @@ public class Ui {
                 + input
                 + colorRed(Style.BORDERLINE.getString());
     }
+
     public void printRedBorderlines(String input) {
         System.out.println(redBorderlines(input));
     }
@@ -73,5 +82,7 @@ public class Ui {
         return "\033[31m" + input + "\033[0m";
     }
 
-    public String colorGreen(String input) { return "\033[33m" + input + "\033[0m"; }
+    public String colorGreen(String input) {
+        return "\033[33m" + input + "\033[0m";
+    }
 }
