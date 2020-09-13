68f47e81b6d6ebdc19a49c0c4c48bdc36752b75c
e0260222 <e0260222@u.nus.edu>
2020-09-13 13:45:45 +0800

    Add JUnit Tests

---
+++ src/test/java/duke/ParserTest.java
@@ -0,0 +1,28 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ *JUnit tests for Parser class
+ */
+public class ParserTest {
+    @Test
+    public void testGetCommand() {
+        assertEquals("todo", Parser.getCommand("todo Eat breakfast"));
+        assertEquals("", Parser.getCommand(""));
+    }
+
+    @Test
+    public void testGetTaskIndex() throws DukeException {
+        assertEquals(2, Parser.getTaskIndex("delete 3"));
+        assertThrows(DukeException.class, () -> {
+            Parser.getTaskIndex("delete");
+        });
+        assertThrows(DukeException.class, () -> {
+            Parser.getTaskIndex("delete x");
+        });
+    }
+}
