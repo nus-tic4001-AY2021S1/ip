f9e7d482e4d612219badec3aa3cddb4a5a016b2c
linqing42 <linqing42@hotmail.com>
2020-09-14 23:51:14 +0800

    handle error for time and date in deadline and event

--- src/main/java/Dukes/Storage.java
+++ src/main/java/Dukes/Storage.java
@@ -9,8 +9,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
