a25751bc193098221721e756fc8faf83083abad5
Lai Ping <>
2020-09-17 22:28:24 +0800

    A-MoreOOP: Use More OOP

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -12,6 +12,7 @@ public class Duke {
 
     public static void main(String[] args) throws IOException, DukeException {
 
+        //private ui ui;
         ArrayList<Task> tasks = new ArrayList<>();
 
         try {
