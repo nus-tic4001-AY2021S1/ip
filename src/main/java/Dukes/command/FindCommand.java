4732d8978d62b2969769fe5348dd7cdedde9c859
linqing42 <linqing42@hotmail.com>
2020-09-20 21:36:53 +0800

    latest update

--- src/main/java/Dukes/command/FindCommand.java
+++ src/main/java/Dukes/command/FindCommand.java
@@ -14,36 +14,39 @@ public class FindCommand extends Command {
     public FindCommand(String keyWord) {
         this.keyWord = keyWord;
     }
+
     /**
      * Finds the tasks with the given keyword.
-     * @param tasks TaskList to be appended.
-     * @param ui UI to interact with user.
+     *
+     * @param tasks   TaskList to be appended.
+     * @param ui      UI to interact with user.
      * @param storage Storage to read and write files.
      */
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
         TaskList filteredTasks = new TaskList();
-        int count =0;
+        int count = 0;
         try {
-            for (Task task :tasks.list) {
+            for (Task task : tasks.list) {
                 String description = task.getDescription().toLowerCase();
                 if (description.contains(keyWord)) {
                     filteredTasks.list.add(task);
+                    count += 1;
                 }
             }
-            if (tasks.isEmpty() || count ==0) {
-                throw new DukeException("There is no task in the list");
-            } else {
-                System.out.println("Here are the matching tasks in your list:");
-                for (int i = 0; i < filteredTasks.list.size(); i++) {
-                    System.out.println((i + 1) + "." + filteredTasks.list.get(i));
+                if (count == 0) {
+                    throw new DukeException("There is no task in the list");
+                } else {
+                    System.out.println("Here are the matching tasks in your list:");
+                    for (int i = 0; i < filteredTasks.list.size(); i++) {
+                        System.out.println((i + 1) + "." + filteredTasks.list.get(i));
+                    }
                 }
-            }
-        } catch (IndexOutOfBoundsException e) {
+            }catch (IndexOutOfBoundsException e) {
             System.out.print("current task is empty in your list.");
         }
+        }
     }
-}
 
 
