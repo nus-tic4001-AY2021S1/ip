8c7d85c223df5f318438959b1de904863a6e30f8
e0261618 <e0261618@u.nus.edu>
2020-09-26 11:08:00 +0800

    added Junit Test

---
+++ src/test/java/duke/TodoTest.java
@@ -0,0 +1,17 @@
+package duke;
+import duke.Todo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+    @Test
+    public void ToDo() {
+
+        Todo todo1 = new Todo("read book");
+        Todo todo2 = new Todo("return book");
+
+        assertEquals("[T][✘] read book", todo1.toString());
+        assertEquals("[T][✘] return book", todo2.toString());
+    }
+}
