21c8aa649e6755d53e7055d758f05f38c9f61831
Chenduo1412 <e0260233@u.nus.edu>
2020-10-21 21:19:45 +0800

    Solve the path dir creation problem

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -1,20 +1,31 @@
 package Duke;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Duke {
+
     private TaskList tasks;
     public Parser paser;
     public Ui ui;
     private Storage storage;
+    private String filename;
+
     public Duke(String path){
         ui = new Ui();
+        try {
+            createDirectory();
+        }catch(DukeException e) {
+            ui.printError(e);
+        }
+
+        this.filename=path;
+
         storage = new Storage(path);
         try {
             tasks = new TaskList(storage.loadTasks());
@@ -24,6 +35,7 @@ public class Duke {
         }
     }
     public void run(){
+
         paser = new Parser(ui,tasks);
         ui.printLogo();
         ui.greetMessage();
@@ -40,8 +52,24 @@ public class Duke {
             }
         }
     }
+
+    public void createDirectory() throws DukeException {
+        try {
+            Path path = Paths.get("data/");
+            if (!(Files.exists(path))) {
+                Files.createDirectory(path);
+                Files.createFile(Paths.get("data/Duke.txt"));
+                System.out.println("creating...");
+            }else System.out.println("exist...");
+        } catch (IOException e) {
+            throw new DukeException("I have problem creating the save directory!");
+        }
+    }
+
+
     public static void main(String[] args) {
-         new Duke("/Users/chenduo/Desktop/ip/Duke.txt").run();
+        Duke d = new Duke("data/Duke.txt");
+        d.run();
         /*
         LocalDate d1 = LocalDate.parse("2019-12-01");
         LocalDate d2 = LocalDate.parse("2019-12-02");
