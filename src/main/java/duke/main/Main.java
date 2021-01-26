c4d34a2ec07b0d89fb7877c93eacdc8a05698f1a
e0260222 <e0260222@u.nus.edu>
2021-01-26 20:49:51 +0800

    Add a GUI

---
+++ src/main/java/duke/main/Main.java
@@ -0,0 +1,30 @@
+package duke.main;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    Duke duke = new Duke("data/tasks.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.setTitle("Duke, a Personal Assistant Chatbot");
+            stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/images/icon.png")));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/duke/main/MainWindow.java
@@ -0,0 +1,57 @@
+package duke.main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * Controller for MainWindow. Provides the layout for the other controls.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+
+    private Duke duke;
+    private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private final Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        showWelcome();
+    }
+
+    public void setDuke(Duke d) {
+        duke = d;
+    }
+
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = duke.getResponse(input);
+
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
+        userInput.clear();
+    }
+
+    @FXML
+    private void showWelcome() {
+        String outputText = "Hello! I'm Duke, your personal chat bot."
+                + System.lineSeparator()
+                + "What can I do for you?";
+
+        dialogContainer.getChildren().addAll(
+                DialogBox.getDukeDialog(outputText, dukeImage)
+        );
+    }
+}
