9e9015359ce6a763650c4f1a5d3da1548d72bfbf
linqing42 <linqing42@hotmail.com>
2021-01-26 15:26:16 +0800

    update

--- src/main/java/dukes/storage/Storage.java
+++ src/main/java/dukes/storage/Storage.java
@@ -16,11 +16,11 @@ import java.util.Scanner;
  * Represents a storage in the hard disk.
  */
 public class Storage {
-    private ArrayList<String> details = new ArrayList<>();
+    private final ArrayList<String> details = new ArrayList<>();
     public static String path = "data/duke.txt";
 
     public Storage(String path) {
-        this.path = path;
+        Storage.path = path;
     }
 
     public ArrayList<String> load() throws dukes.exception.FileNotFoundException {
@@ -32,15 +32,19 @@ public class Storage {
                 String input = in.nextLine();
                 String[] strArr = input.split(" \\| ");
                 Task tasks;
-                if (strArr[0].equals("T")) {
+                switch (strArr[0]) {
+                case "T":
                     tasks = new ToDo(strArr[2]);
-                } else if (strArr[0].equals("D")) {
+                    break;
+                case "D":
                     tasks = new Deadline(strArr[2], strArr[3]);
-                } else if (strArr[0].equals("E")) {
+                    break;
+                case "E":
                     tasks = new Event(strArr[2], strArr[3]);
-                } else {
+                    break;
+                default:
                     throw new dukes.exception.FileNotFoundException("Previous Tasks are corrupted."
-                            + "Please resetting your task.");
+                                + "Please resetting your task.");
                 }
                 if (strArr[1].equals("1")) {
                     tasks.markAsDone();
@@ -60,11 +64,11 @@ public class Storage {
     public void saveTaskFile(ArrayList<Task> task) {
         try {
             FileWriter fw = new FileWriter("data/duke.txt");
-            String fileDetails = "";
+            StringBuilder fileDetails = new StringBuilder();
             for (Task t : task) {
-                fileDetails += t.toWriteFile() + "\n";
+                fileDetails.append(t.toWriteFile()).append("\n");
             }
-            fw.write(fileDetails);
+            fw.write(fileDetails.toString());
             fw.close();
         } catch (IOException e) {
             System.out.println("FileWriting got problem");
