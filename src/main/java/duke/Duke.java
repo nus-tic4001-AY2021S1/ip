aa2b6316ca490ca858b455e76c491b3c2781d5c5
Adi <e0261621@u.nus.edu>
2021-02-05 02:55:50 +0800

    Testing out JavaFX stuffs

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,12 +6,28 @@ import java.util.ArrayList;
 
 import data.TaskList;
 import exception.DukeException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import parser.Parser;
 import storage.Storage;
 import ui.Constants;
 import ui.Ui;
 
-public class Duke {
+public class Duke extends Application {
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     private Storage storage;
     private TaskList tasks;
@@ -21,152 +37,200 @@ public class Duke {
      * Duke Main.
      * @param filePath
      */
-    public Duke(String filePath) {
-        ui = new Ui();
-        storage = new Storage(filePath);
-        try {
-            tasks = new TaskList(storage.load());
-        } catch (NoSuchFileException | FileNotFoundException | DukeException e) {
+//    public Duke(String filePath) {
+//        ui = new Ui();
+//        storage = new Storage(filePath);
+//        try {
+//            tasks = new TaskList(storage.load());
+//        } catch (NoSuchFileException | FileNotFoundException | DukeException e) {
+//
+//            if (e.toString().contains("FileNotFoundException") || e.toString().contains("NoSuchFileException")) {
+//
+//            } else if (e.toString().contains("DukeException")) {
+//                ui.showToUser(Constants.FILE_NOT_READABLE_EXCEPTION);
+//            }
+//            tasks = new TaskList(new ArrayList<>());
+//        }
+//
+//    }
+//
+//    /**
+//     * Begin running main processing loop
+//     */
+//    public void run() {
+//        ui.printWelcome();
+//
+//        boolean isExit = false;
+//        while (!isExit) {
+//
+//            try {
+//                String fullCommand = ui.readUserCommand();
+//                String commandWord = Parser.getCommandWord(fullCommand);
+//
+//                switch (commandWord.toUpperCase()) {
+//                case "":
+//                    ui.printError("No command input! Please enter a command "
+//                            + "or type \"help\" to view a list of commands.");
+//                    break;
+//
+//                case "EXIT":
+//                    isExit = true;
+//                    if (tasks.getNumberOfTasks() > 0) {
+//                        storage.save(tasks.getTasks());
+//                    }
+//                    ui.endInputFeed();
+//                    ui.printGoodBye();
+//                    break;
+//
+//                case "HELP":
+//                    ui.helpMessage();
+//                    Ui.showToUser(Constants.H_LINE);
+//                    break;
+//
+//                case "TODO":
+//                    tasks.addTask(Parser.createTodo(fullCommand));
+//                    ui.printTask(tasks.getTasks().get(TaskList.getNumberOfTasks() - 1), Constants.TaskDisplayType.ADD);
+//                    Ui.printNumberOfTasks(TaskList.getNumberOfTasks());
+//                    Ui.showToUser(Constants.H_LINE);
+//                    storage.save(tasks.getTasks());
+//                    break;
+//
+//                case "DEADLINE":
+//                    tasks.addTask(Parser.createDeadLine(fullCommand));
+//                    ui.printTask(tasks.getTasks().get(TaskList.getNumberOfTasks() - 1), Constants.TaskDisplayType.ADD);
+//                    Ui.printNumberOfTasks(TaskList.getNumberOfTasks());
+//                    Ui.showToUser(Constants.H_LINE);
+//                    storage.save(tasks.getTasks());
+//                    break;
+//
+//                case "EVENT":
+//                    tasks.addTask(Parser.createEvent(fullCommand));
+//                    ui.printTask(tasks.getTasks().get(TaskList.getNumberOfTasks() - 1), Constants.TaskDisplayType.ADD);
+//                    Ui.printNumberOfTasks(TaskList.getNumberOfTasks());
+//                    Ui.showToUser(Constants.H_LINE);
+//                    storage.save(tasks.getTasks());
+//                    break;
+//
+//                case "VIEW":
+//                    if (TaskList.getNumberOfTasks() > 0) {
+//
+//                        ui.printTasks(tasks.getTasks());
+//                    } else {
+//                        Ui.showToUser(Constants.NO_TASKS + "\n" + Constants.H_LINE);
+//                    }
+//
+//                    break;
+//
+//                case "FIND":
+//                    if (tasks.findTasks(Parser.getKeyword(fullCommand)).size() > 0) {
+//                        ui.printFoundTasks(tasks.findTasks(Parser.getKeyword(fullCommand)));
+//                    } else {
+//                        Ui.showToUser(Constants.NO_MATCHING_TASKS);
+//                    }
+//                    break;
+//
+//                case "DONE":
+//                    try {
+//                        tasks.markAsDone(Parser.parseTaskNum(fullCommand));
+//                        ui.printTask(tasks.getTasks().get(Parser.parseTaskNum(fullCommand) - 1),
+//                                Constants.TaskDisplayType.DONE);
+//                        Ui.showToUser(Constants.H_LINE);
+//                        storage.save(tasks.getTasks());
+//                    } catch (IndexOutOfBoundsException idxE) {
+//                        Ui.showToUser(Constants.INVALIDIDXINPUT + "\n" + Constants.H_LINE);
+//                    }
+//                    break;
+//
+//                case "UNDONE":
+//                    try {
+//                        tasks.markAsNotDone(Parser.parseTaskNum(fullCommand));
+//                        ui.printTask(tasks.getTasks().get(Parser.parseTaskNum(fullCommand) - 1),
+//                                Constants.TaskDisplayType.UNDONE);
+//                        Ui.showToUser(Constants.H_LINE);
+//                        storage.save(tasks.getTasks());
+//                    } catch (IndexOutOfBoundsException idxE) {
+//                        Ui.showToUser(Constants.INVALIDIDXINPUT + "\n" + Constants.H_LINE);
+//                    }
+//                    break;
+//
+//                case "REMOVE":
+//                    try {
+//                        tasks.removeTask(fullCommand);
+//                        Ui.printNumberOfTasks(TaskList.getNumberOfTasks());
+//                        Ui.showToUser(Constants.H_LINE);
+//                        storage.save(tasks.getTasks());
+//                    } catch (IndexOutOfBoundsException idxE) {
+//                        Ui.showToUser(Constants.INVALIDIDXINPUT + "\n" + Constants.H_LINE);
+//                    }
+//                    break;
+//
+//                case "SAVE":
+//                    Ui.printNumberOfTasks(TaskList.getNumberOfTasks());
+//                    storage.save(tasks.getTasks());
+//                    Ui.showToUser(Constants.SAVE_TASKS_ACKNOWLEGEMENT);
+//                    break;
+//
+//                default:
+//                    ui.printError("Unknown command! Please try again. Type \"help\" to view a list of commands.");
+//                    break;
+//                }
+//            } catch (Exception e) {
+//                ui.printError(e.getMessage());
+//            }
+//        }
+//        System.exit(0);
+//    }
+//
+//    public static void main(String[] args) {
+//        new Duke("data/tasks.txt").run();
+//    }
 
-            if (e.toString().contains("FileNotFoundException") || e.toString().contains("NoSuchFileException")) {
+    @Override
+    public void start(Stage stage) {
 
-            } else if (e.toString().contains("DukeException")) {
-                ui.showToUser(Constants.FILE_NOT_READABLE_EXCEPTION);
-            }
-            tasks = new TaskList(new ArrayList<>());
-        }
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
 
-    }
+        userInput = new TextField();
+        sendButton = new Button("Send");
 
-    /**
-     * Begin running main processing loop
-     */
-    public void run() {
-        ui.printWelcome();
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
-        boolean isExit = false;
-        while (!isExit) {
+        scene = new Scene(mainLayout);
 
-            try {
-                String fullCommand = ui.readUserCommand();
-                String commandWord = Parser.getCommandWord(fullCommand);
+        stage.setScene(scene);
+        stage.show();
 
-                switch (commandWord.toUpperCase()) {
-                case "":
-                    ui.printError("No command input! Please enter a command "
-                            + "or type \"help\" to view a list of commands.");
-                    break;
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
 
-                case "EXIT":
-                    isExit = true;
-                    if (tasks.getNumberOfTasks() > 0) {
-                        storage.save(tasks.getTasks());
-                    }
-                    ui.endInputFeed();
-                    ui.printGoodBye();
-                    break;
+        mainLayout.setPrefSize(400.0, 600.0);
 
-                case "HELP":
-                    ui.helpMessage();
-                    Ui.showToUser(Constants.H_LINE);
-                    break;
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
 
-                case "TODO":
-                    tasks.addTask(Parser.createTodo(fullCommand));
-                    ui.printTask(tasks.getTasks().get(TaskList.getNumberOfTasks() - 1), Constants.TaskDisplayType.ADD);
-                    Ui.printNumberOfTasks(TaskList.getNumberOfTasks());
-                    Ui.showToUser(Constants.H_LINE);
-                    storage.save(tasks.getTasks());
-                    break;
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
 
-                case "DEADLINE":
-                    tasks.addTask(Parser.createDeadLine(fullCommand));
-                    ui.printTask(tasks.getTasks().get(TaskList.getNumberOfTasks() - 1), Constants.TaskDisplayType.ADD);
-                    Ui.printNumberOfTasks(TaskList.getNumberOfTasks());
-                    Ui.showToUser(Constants.H_LINE);
-                    storage.save(tasks.getTasks());
-                    break;
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
 
-                case "EVENT":
-                    tasks.addTask(Parser.createEvent(fullCommand));
-                    ui.printTask(tasks.getTasks().get(TaskList.getNumberOfTasks() - 1), Constants.TaskDisplayType.ADD);
-                    Ui.printNumberOfTasks(TaskList.getNumberOfTasks());
-                    Ui.showToUser(Constants.H_LINE);
-                    storage.save(tasks.getTasks());
-                    break;
+        userInput.setPrefWidth(325.0);
 
-                case "VIEW":
-                    if (TaskList.getNumberOfTasks() > 0) {
+        sendButton.setPrefWidth(55.0);
 
-                        ui.printTasks(tasks.getTasks());
-                    } else {
-                        Ui.showToUser(Constants.NO_TASKS + "\n" + Constants.H_LINE);
-                    }
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
 
-                    break;
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
 
-                case "FIND":
-                    if (tasks.findTasks(Parser.getKeyword(fullCommand)).size() > 0) {
-                        ui.printFoundTasks(tasks.findTasks(Parser.getKeyword(fullCommand)));
-                    } else {
-                        Ui.showToUser(Constants.NO_MATCHING_TASKS);
-                    }
-                    break;
-
-                case "DONE":
-                    try {
-                        tasks.markAsDone(Parser.parseTaskNum(fullCommand));
-                        ui.printTask(tasks.getTasks().get(Parser.parseTaskNum(fullCommand) - 1),
-                                Constants.TaskDisplayType.DONE);
-                        Ui.showToUser(Constants.H_LINE);
-                        storage.save(tasks.getTasks());
-                    } catch (IndexOutOfBoundsException idxE) {
-                        Ui.showToUser(Constants.INVALIDIDXINPUT + "\n" + Constants.H_LINE);
-                    }
-                    break;
-
-                case "UNDONE":
-                    try {
-                        tasks.markAsNotDone(Parser.parseTaskNum(fullCommand));
-                        ui.printTask(tasks.getTasks().get(Parser.parseTaskNum(fullCommand) - 1),
-                                Constants.TaskDisplayType.UNDONE);
-                        Ui.showToUser(Constants.H_LINE);
-                        storage.save(tasks.getTasks());
-                    } catch (IndexOutOfBoundsException idxE) {
-                        Ui.showToUser(Constants.INVALIDIDXINPUT + "\n" + Constants.H_LINE);
-                    }
-                    break;
-
-                case "REMOVE":
-                    try {
-                        tasks.removeTask(fullCommand);
-                        Ui.printNumberOfTasks(TaskList.getNumberOfTasks());
-                        Ui.showToUser(Constants.H_LINE);
-                        storage.save(tasks.getTasks());
-                    } catch (IndexOutOfBoundsException idxE) {
-                        Ui.showToUser(Constants.INVALIDIDXINPUT + "\n" + Constants.H_LINE);
-                    }
-                    break;
-
-                case "SAVE":
-                    Ui.printNumberOfTasks(TaskList.getNumberOfTasks());
-                    storage.save(tasks.getTasks());
-                    Ui.showToUser(Constants.SAVE_TASKS_ACKNOWLEGEMENT);
-                    break;
-
-                default:
-                    ui.printError("Unknown command! Please try again. Type \"help\" to view a list of commands.");
-                    break;
-                }
-            } catch (Exception e) {
-                ui.printError(e.getMessage());
-            }
-        }
-        System.exit(0);
-    }
-
-    public static void main(String[] args) {
-        new Duke("data/tasks.txt").run();
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
     }
 
 }
